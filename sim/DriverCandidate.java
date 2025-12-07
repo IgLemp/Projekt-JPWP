7f89bbed53cd3d8cdb85f84111b30b351a8b5dbc
iglemp <iglemp@protonmail.com>
2025-12-07 12:51:41 +0100

    More Lombokification

--- sim/DriverCandidate.java
+++ sim/DriverCandidate.java
@@ -2,7 +2,9 @@ package com.transport.sim;
 
 import java.util.*;
 import lombok.Getter;
+import lombok.AllArgsConstructor;
 
+@AllArgsConstructor
 public class DriverCandidate {
     @Getter private String name;
     @Getter private int skill;
@@ -11,10 +13,6 @@ public class DriverCandidate {
     private static final String[] FIRST = {"Adam","Piotr","Marek","Krzysztof","Paweł","Jakub","Łukasz","Tomasz","Mateusz","Szymon"};
     private static final String[] LAST = {"Nowak","Kowalski","Wiśniewski","Wójcik","Kowalczyk","Kamiński","Lewandowski","Zieliński"};
     private static final Random rng = new Random();
-
-    public DriverCandidate(String name, int skill, double hireCost) {
-        this.name = name; this.skill = skill; this.hireCost = hireCost;
-    }
 
     public static DriverCandidate randomCandidate() {
         String name = FIRST[rng.nextInt(FIRST.length)] + " " + LAST[rng.nextInt(LAST.length)];
